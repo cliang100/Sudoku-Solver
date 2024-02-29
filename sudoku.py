c2fd2aadbd4848898ad65eaaab587671a9ed246e
Chris <98251425+cliang100@users.noreply.github.com>
2024-02-29 13:14:18 -0500

    Update sudoku.py
    improving sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -1,60 +1,103 @@
-def finds_next_empty(puzzle):
-    for r in range(9):
-        for c in range(9):
-            if puzzle[r][c] == -1:
-                return r, c
-    return None, None
+class Board:
+    def __init__(self, board):
+        self.board = board
 
-def is_valid(puzzle, guess, row, col):
-    row_vals = puzzle[row]
-    if guess in row_vals:
-        return False
-    
-    # col_vals = []
-    # for i in range(9):
-    #     col_vals.append(puzzle[i][col])
-    col_vals = [puzzle[i][col] for i in range(9)]
-    if guess in col_vals:
-        return False
-    
-    row_start = (row // 3) * 3
-    col_start = (col // 3) * 3
+    def __str__(self):
+        upper_lines = f'\n╔═══{"╤═══"*2}{"╦═══"}{"╤═══"*2}{"╦═══"}{"╤═══"*2}╗\n'
+        middle_lines = f'╟───{"┼───"*2}{"╫───"}{"┼───"*2}{"╫───"}{"┼───"*2}╢\n'
+        lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
+        board_string = upper_lines
+        for index, line in enumerate(self.board):
+            row_list = []
+            for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start=1):
+                row_square = '|'.join(str(item) for item in part)
+                row_list.extend(row_square)
+                if square_no != 3:
+                    row_list.append('║')
 
-    for r in range(row_start, row_start + 3):
-        for c in range(col_start, col_start + 3):
-            if puzzle[r][c] == guess:
-                return False
-            
-    return True
+            row = f'║ {" ".join(row_list)} ║\n'
+            row_empty = row.replace('0', ' ')
+            board_string += row_empty
 
-def solve_sudoku(puzzle):
-    row, col = finds_next_empty(puzzle)
+            if index < 8:
+                if index % 3 == 2:
+                    board_string += f'╠═══{"╪═══"*2}{"╬═══"}{"╪═══"*2}{"╬═══"}{"╪═══"*2}╣\n'
+                else:
+                    board_string += middle_lines
+            else:
+                board_string += lower_lines
 
-    if row is None:
+        return board_string
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        return all(
+            self.board[row][col] != num
+            for row in range(9)
+        )
+
+    def valid_in_square(self, row, col, num):
+        row_start = (row // 3) * 3
+        col_start=(col // 3) * 3
+        for row_no in range(row_start, row_start + 3):
+            for col_no in range(col_start, col_start + 3):
+                if self.board[row_no][col_no] == num:
+                    return False
         return True
-    for guess in range(1,10):
-        if is_valid(puzzle, guess, row, col):
-            puzzle[row][col] = guess
-            if solve_sudoku(puzzle):
-                return True
-            
-        puzzle[row][col] = -1
-    
-    return False
+
+    def is_valid(self, empty, num):
+        row, col = empty
+        valid_in_row = self.valid_in_row(row, num)
+        valid_in_col = self.valid_in_col(col, num)
+        valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
+
+    def solver(self):
+        if (next_empty := self.find_empty_cell()) is None:
+            return True
+        else:
+            for guess in range(1, 10):
+                if self.is_valid(next_empty, guess):
+                    row, col = next_empty
+                    self.board[row][col] = guess
+                    if self.solver():
+                        return True
+                    self.board[row][col] = 0
+
+        return False
+
+def solve_sudoku(board):
+    gameboard = Board(board)
+    print(f'\nPuzzle to solve:\n{gameboard}')
+    if gameboard.solver():
+        print('\nSolved puzzle:')
+        print(gameboard)
+
+    else:
+        print('\nThe provided puzzle is unsolvable.')
+    return gameboard
+puzzle = [
+  [0, 0, 2, 0, 0, 8, 0, 0, 0],
+  [0, 0, 0, 0, 0, 3, 7, 6, 2],
+  [4, 3, 0, 0, 0, 0, 8, 0, 0],
+  [0, 5, 0, 0, 3, 0, 0, 9, 0],
+  [0, 4, 0, 0, 0, 0, 0, 2, 6],
+  [0, 0, 0, 4, 6, 7, 0, 0, 0],
+  [0, 8, 6, 7, 0, 4, 0, 0, 0],
+  [0, 0, 0, 5, 1, 9, 0, 0, 8],
+  [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
 
 if __name__ == '__main__':
-    example_board = [
-        [-1, -1, -1,   -1, 6, 4,   2, -1, 9],
-        [-1, -1, -1,   -1, 9, -1,   -1, -1, 4],
-        [7, 4, 9,   1, 3, 2,   -1, -1, -1],
-
-        [2, -1, -1,   -1, -1, -1,   8, -1, -1],
-        [-1, -1, 5,   -1, -1, 1,   -1, -1, -1],
-        [9, 8, -1,   -1, -1, 6,   -1, 7, -1],
-
-        [-1, -1, -1,   4, -1, -1,   5, -1, 8],
-        [-1, -1, 7,   9, 1, -1,   -1, -1, -1],
-        [-1, 2, -1,   -1, 8, -1,   -1, -1, -1]
-    ]
-    print(solve_sudoku(example_board))
-    print(example_board)
+    solve_sudoku(puzzle)
